e40207197310a9f7c4a955b88d20448dc9a73c61
Alexis-Alvarez-R <ramirez151202@gmail.com>
2026-01-02 17:47:22 -0600

    fixed ruta

--- src/helpers/downloadCV.ts
+++ src/helpers/downloadCV.ts
@@ -1,6 +1,6 @@
 export const downloadCV = () => {
   const link = document.createElement("a");
-  link.href = "/public/cv/alexis-alvarez-cv.pdf";
+  link.href = "/cv/alexis-alvarez-cv.pdf";
   link.download = "alexis-alvarez-cv.pdf";
   document.body.appendChild(link);
   link.click();
