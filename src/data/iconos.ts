28a8afb0da17f17616af99739ad4803c245e2d6d
Alexis-Alvarez-R <ramirez151202@gmail.com>
2026-01-02 17:19:46 -0600

    change img

--- src/data/iconos.ts
+++ src/data/iconos.ts
@@ -1,3 +1,7 @@
+import iconGithub from "../assets/iconos/icons/github.svg";
+import iconLinkedin from "../assets/iconos/icons/linkedin.svg";
+import iconIG from "../assets/iconos/icons/instagram.svg";
+
 interface arrayIconos {
   id: number;
   url: string;
@@ -7,17 +11,17 @@ interface arrayIconos {
 export const iconos: arrayIconos[] = [
   {
     id: 1,
-    url: "/public/assets/icons/github.svg",
+    url: iconGithub,
     link: "https://github.com/Alexis-Alvarez-R",
   },
   {
     id: 2,
-    url: "/public/assets/icons/linkedin.svg",
+    url: iconLinkedin,
     link: "https://www.linkedin.com/in/alexis-alvarez-28863a396/",
   },
   {
     id: 3,
-    url: "/public/assets/icons/instagram.svg",
+    url: iconIG,
     link: "https://www.instagram.com/alexis_alvare5?igsh=MTJ0YzJteDAyemI2ZQ==",
   },
 ];
