21b1c1fc75ed68889dc737ca01520700195ed0fa
Alexis-Alvarez-R <ramirez151202@gmail.com>
2026-01-09 15:32:43 -0600

    new certification

--- src/data/certificaciones.ts
+++ src/data/certificaciones.ts
@@ -12,7 +12,12 @@ export const certifications: Certificacion[] = [
     credentialURL: "https://www.udemy.com/certificate/UC-0bb475f2-6685-4f24-8418-ccdebcf32ab0/",
   },
   {
-    title: "Nest:Desarrollo backend escalable con Node.",
+    title: "React: De cero a experto",
+    issuer: "Udemy",
+    credentialURL: "https://www.udemy.com/certificate/UC-c4545602-a04a-474c-9107-4af7ce281be9/",
+  },
+  {
+    title: "Nest: Desarrollo backend escalable con Node.",
     issuer: "Udemy",
     credentialURL: "https://www.udemy.com/certificate/UC-ace68d38-6a91-4c29-98a1-71f88a489eb2/",
   },
